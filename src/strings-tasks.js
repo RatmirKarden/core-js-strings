1ed5855ac595070484bb253cddc6ac5bc4cc2fcd
RatmirKarden <ratmir_kardanov@list.ru>
2025-09-20 22:55:22 +0300

    feat:decide tasks,next 9 tasks continued tommorrow

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -328,8 +328,9 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const pal = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return pal === pal.split('').reverse().join('');
 }
 
 /**
